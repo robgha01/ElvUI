c401ebc15629c2ee774d4a249dbed5f8f6e6d683
robgha01 <robertghafoor@hotmail.com>
2018-09-30 18:21:44 +0200

    initial commit

---
+++ Skins/Addons/chocolateBar.lua
@@ -0,0 +1,20 @@
+local E, L, V, P, G, _ = unpack(ElvUI)
+local AS = E:GetModule("AddOnSkins")
+local S = E:GetModule("Skins")
+
+local function LoadSkin()
+	if(not E.private.addOnSkins.ChocolateBar) then return end
+	AS:UpdateMedia()
+
+	for i = 1, 20 do 	
+		local f = _G['ChocolateBar'..i]
+		if f then
+			AS:SkinFrame(f, 'Default')
+		end
+	end
+	if RaidUtility_ShowButton then
+		RaidUtility_ShowButton:SetFrameStrata('TOOLTIP')
+	end
+end
+
+S:AddCallbackForAddon("ChocolateBar", "ChocolateBar", LoadSkin)
